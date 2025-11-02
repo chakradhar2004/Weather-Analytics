c1d9677d1a593fc79688390fef58039480ce3aa2
Your Name <kakkuluruchakradhargoud@gmail.com>
2025-11-02 08:15:44 +0000

    ✓ Starting...  ⚠ Invalid next.config.ts options detected:  ⚠     Unreco

---
+++ src/next.config.ts
@@ -0,0 +1,49 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    // This is a development-only option, it will not be included in production builds.
+    // It allows requests from the specified origins to access the development server.
+    allowedDevOrigins: [
+      'https://*.cloudworkstations.dev',
+      'https://*.firebase.studio',
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.weatherapi.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
